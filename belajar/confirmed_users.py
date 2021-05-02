07ecbc505d7f4963837c6a1693a56c5e4060a964
mardin <mardin@localhost.localdomain>
2021-05-02 17:46:46 +0700

    initial commit

--- belajar/confirmed_users.py
+++ belajar/confirmed_users.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 unconfirmed_users = ['alice', 'brian', 'candace']
 confirmed_users = []
+
+# Verify each user until there are no more unconfirmed users.
+# Move each verified user into the list of confirmed users
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print(f"Verifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
