3e4738460d803f4871fb8ddc384405975e98968d
Developer Arto <developer@localhost.localdomain>
2021-08-05 16:41:39 +0700

    initial commit

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,5 +1,6 @@
 import unittest
 import time
+from datetime import datetime
 from absen import create_app, db
 from absen.models import User, Permission, Role, AnonymousUser
 
@@ -122,3 +123,22 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(u.can(Permission.COMMENT))
         self.assertFalse(u.can(Permission.MODERATE))
         self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_timestamps(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        self.assertTrue(
+            (datetime.utcnow() - u.member_since).total_seconds() < 3)
+        self.assertTrue(
+            (datetime.utcnow() - u.last_seen).total_seconds() < 3)
+
+    def test_ping(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        time.sleep(2)
+        last_seen_before = u.last_seen
+        u.ping()
+        self.assertTrue(u.last_seen > last_seen_before)
+
