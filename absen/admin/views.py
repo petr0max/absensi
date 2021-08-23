34ca5701590aff3b00ed4b323c9d04b7bd2c2a19
mardin <mardin@localhost.localdomain>
2021-08-23 20:18:41 +0700

    initial commit

--- absen/admin/views.py
+++ absen/admin/views.py
@@ -1,3 +1,14 @@
-from flask_login import login_required
+from flask import request, url_for, flash, redirect
+from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
 from . import adm
+
+
+@adm.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        if current_user.is_administrator \
+                and request.endpoint \
+                and request.blueprint != 'adm' \
+                and request.endpoint != 'static':
+            return redirect(url_for('main.index'))
