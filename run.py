b65042cb4bd8b36dd40a6c772009fe7357157a91
devarto <dev@artolanggengrejeki.com>
2021-08-27 06:10:47 +0000

    initial commit

--- run.py
+++ run.py
@@ -20,3 +20,7 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == "__main__":
+    app.run()
